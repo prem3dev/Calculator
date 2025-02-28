1929970ae2ad0ad25bae6f9ffd318d2035c46805
prem3dev <ecstasy1508@gmail.com>
2025-02-28 15:32:49 +0900

    기존 클래스를 Main으로 수정 후 Calculator클래스 신규 생성

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,34 @@
 package calculator;
 
 public class Calculator {
+    //속성
+    double result;
+
+//생성자
+
+    //기능 더하기 빼기 곱하기 나누기
+
+    public double sum(double nuber1, double number2) {
+        this.result = nuber1 + number2;
+        return this.result;
+    }
+
+    public double sub(double nuber1, double number2) {
+        this.result = nuber1 - number2;
+        return this.result;
+    }
+
+    public double mul(double nuber1, double number2) {
+        this.result = nuber1 * number2;
+        return this.result;
+    }
+
+    public double div(double nuber1, double number2) {
+        this.result = nuber1 / number2;
+        return this.result;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
 }

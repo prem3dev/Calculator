43e13aca5fd3a941abdf3314b75ce6b4e5770324
prem3dev <ecstasy1508@gmail.com>
2025-02-28 20:03:11 +0900

    정렬

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator();
         System.out.print("숫자를 입력하세요: ");
 
         try {

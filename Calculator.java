10c7f7dc9a71ba78850fd94d76de0bc988c6db00
prem3dev <ecstasy1508@gmail.com>
2025-03-05 11:35:16 +0900

    ìLV2 완성진

--- Calculator.java
+++ Calculator.java
@@ -1,38 +1,139 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Calculator {
     //속성
-    private double result;
+    private ArrayList<Double> results = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
 //생성자
 
-    //기능 더하기 빼기 곱하기 나누기
+    //기능 더하기 빼기 곱하기 나누기 게터 세터
 
-    public double sum(double nuber1, double number2) {
-        this.result = nuber1 + number2;
-        return this.result;
-    }
-
-    public double sub(double nuber1, double number2) {
-        this.result = nuber1 - number2;
-        return this.result;
-    }
-
-    public double mul(double nuber1, double number2) {
-        this.result = nuber1 * number2;
-        return this.result;
-    }
-
-    public double div(double nuber1, double number2) {
-        this.result = nuber1 / number2;
-        return this.result;
-    }
-
-    public double getResult() {
-        return this.result;
-    }
-
-    public void setResult(double result) {
-        this.result = result;
+    public double calculate(double number1, char operator, double number2) throws ArithmeticException {
+        if (results.isEmpty()) {
+            if (operator == '+') {
+                double sumResult = number1 + number2;
+                results.add(sumResult);
+                return sumResult;
+            } else if (operator == '-') {
+                double subResult = number1 - number2;
+                results.add(subResult);
+                return subResult;
+            } else if (operator == '*') {
+                double mulResult = number1 * number2;
+                results.add(mulResult);
+                return mulResult;
+            } else {
+                if (number2 == 0) {
+                    throw new ArithmeticException();
+                }
+                double divResult = number1 / number2;
+                results.add(divResult);
+                return divResult;
+            }
+        } else {
+            int lastIndex = results.size() - 1;
+            if (operator == '+') {
+                double sumResult = results.get(lastIndex) + number2;
+                results.add(sumResult);
+                return sumResult;
+            } else if (operator == '-') {
+                double subResult = results.get(lastIndex) - number2;
+                results.add(subResult);
+                return subResult;
+            } else if (operator == '*') {
+                double mulResult = results.get(lastIndex) * number2;
+                results.add(mulResult);
+                return mulResult;
+            } else {
+                if (number2 == 0) {
+                    throw new ArithmeticException();
+                }
+                double divResult = results.get(lastIndex) / number2;
+                results.add(divResult);
+                return divResult;
+            }
         }
     }
+
+    public void printResult() {
+        while (true) {
+            System.out.println("몇 번째 연산을 조회 하시겠습니까? back 입력시 접근 진행 창으로");
+            String index = scanner.next();
+
+            if (index.equals("back")) {
+                break;
+            } else {
+                try {
+                    int indexNumber = Integer.parseInt(index) - 1;
+                    double searchResult = results.get(indexNumber);
+                    System.out.println("조회 결과 : " + searchResult);
+                    break;
+                } catch (Exception e) {
+                    System.out.println("잘못된 입력입니다.");
+                }
+            }
+        }
+    }
+
+    public void setResult() {
+        while (true) {
+            System.out.println("몇 번째 연산을 수정 하시겠습니까? back 입력 시 접근 진행 창으로");
+            String index = scanner.next();
+
+            if (index.equals("back")) {
+                break;
+            } else {
+                while (true) {
+                    try {
+                        int indexNumber = Integer.parseInt(index) - 1;
+                        System.out.println("수정 정보를 입력하세요. (back 입력 시 접근 진행 창으로)");
+                        String value = scanner.next();
+                        if (index.equals("back")) {
+                            break;
+                        } else {
+                            try {
+                                double correctionValue = Double.parseDouble(value);
+                                results.set(indexNumber, correctionValue);
+                                System.out.println("수정이 완료되었습니다.");
+                                return;
+                            } catch (Exception e) {
+                                System.out.println("잘못된 입력입니다.");
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("잘못된 입력입니다.");
+                    }
+                }
+            }
+        }
+    }
+
+    public void removeResult() {
+        while (true) {
+            System.out.println("몇 번째 연산을 삭제 하시겠습니까? back 입력시 접근 진행 창으로");
+            String delete = scanner.next();
+
+            if (delete.equals("back")) {
+                break;
+            } else {
+                try {
+                    int deleteIndexNumber = Integer.parseInt(delete) - 1;
+                    results.remove(deleteIndexNumber);
+                    System.out.println("삭제가 완료되었습니다.");
+                    return;
+                } catch (Exception e) {
+                    System.out.println("잘못된 입력입니다.");
+                }
+            }
+        }
+    }
+
+    void clearResult() {
+        results.clear();
+    }
+}
+
--- Main.java
+++ Main.java
@@ -8,70 +8,72 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        System.out.print("숫자를 입력하세요: ");
 
-        try {
-            double numberA = scanner.nextDouble();
-            while (true) {
-                System.out.print("사칙연산 기호를 입력하세요(exit 입력 시 종료): ");
+        while (true) {
+            System.out.print("숫자를 입력하세요\n(\"exit\" 입력 시 - 계산기 종료)\n입력 : ");
+            String textNumberA = scanner.next();
+            if (textNumberA.equals("exit")) {
+                break;
+            } else {
+                try {
+                    double numberA = Double.parseDouble(textNumberA);
+                    while (true) {
+                        System.out.print("사칙연산 기호를 입력하세요\n(\"clear\" 입력 시 - 초기화)\n(\"조회\" 입력 시 - 조회하기)\n(\"수정\" 입력 시 - 수정하기)\n(\"삭제\" 입력 시 - 삭제하기)\n(\"exit\" 입력 시 - 계산기 종료)\n입력 : ");
 
-                String text = scanner.next();
+                        String textOperator = scanner.next();
 
-                if (text.equals("exit")) {
-                    break;
-                } else if (
-                        text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/")) {
+                        if (textOperator.equals("clear")) {
+                            calculator.clearResult();
+                            break;
+                        } else if (textOperator.equals("exit")) {
+                            return;
+                        } else if (textOperator.equals("조회")) {
+                            calculator.printResult();
+                        } else if (textOperator.equals("수정")) {
+                            calculator.setResult();
+                        } else if (textOperator.equals("삭제")) {
+                            calculator.removeResult();
+                        } else if (textOperator.equals("+") || textOperator.equals("-") || textOperator.equals("*") || textOperator.equals("/")) {
+                            char operator = textOperator.charAt(0);
+                            while (true) {
+                                System.out.print("숫자를 입력하세요\n(\"back\" 입력 시 - 이전으로)\n(\"조회\" 입력 시 - 조회하기)\n(\"수정\" 입력 시 - 수정하기)\n(\"삭제\" 입력 시 - 삭제하기)\n(\"exit\" 입력 시 - 계산기 종료)\n입력 : ");
+                                String textNumberB = scanner.next();
 
-                    char operator = text.charAt(0);
-                    System.out.print("숫자를 입력하세요(exit 입력 시 종료): ");
-                    String textNumberB = scanner.next();
-
-                    if (textNumberB.equals("exit")) {
-                        break;
-                    } else {
-                        try {
-                            double numberB = Double.parseDouble(textNumberB);
-
-                            switch (operator) {
-                                case '+':
-                                    double result = calculator.sum(numberA, numberB);
-                                    numberA = result;
-                                    System.out.println("result : " + result);
+                                if (textNumberB.equals("back")) {
                                     break;
-                                case '-':
-                                    result = calculator.sub(numberA, numberB);
-                                    numberA = result;
-                                    System.out.println("result : " + result);
-                                    break;
-                                case '*':
-                                    result = calculator.mul(numberA, numberB);
-                                    numberA = result;
-                                    System.out.println("result : " + result);
-                                    break;
-                                case '/':
-                                    if (numberB == 0) {
-                                        System.out.println("0 이외에 다른 수를 다시 입력하세요");
-                                        continue;
-                                    } else {
-                                        result = calculator.div(numberA, numberB);
-                                        numberA = result;
-                                        System.out.println("result : " + result);
-                                        break;
+                                }  else if (textNumberB.equals("exit")) {
+                                    return;
+                                } else if (textNumberB.equals("조회")) {
+                                    calculator.printResult();
+                                } else if (textNumberB.equals("수정")) {
+                                    calculator.setResult();
+                                } else if (textNumberB.equals("삭제")) {
+                                    calculator.removeResult();
+                                } else {
+                                    try {
+                                        double numberB = Double.parseDouble(textNumberB);
+                                        try {
+                                            double result = calculator.calculate(numberA, operator, numberB);
+                                            System.out.println(result);
+                                            ;break;
+                                        } catch (ArithmeticException e) {
+                                            System.out.println("0으로 나눌 수 없습니다.");
+                                            continue;
+                                        }
+                                    } catch (Exception e) {
+                                        System.out.println("올바르지 않은 입력입니다.");
                                     }
+                                }
                             }
-                        } catch (Exception e) {
-                            System.out.println("올바르지 않은 입력입니다.");
+                        } else {
+                            System.out.println("올바르지않은 기호입니다.");
                             continue;
                         }
                     }
-                } else {
-                    System.out.println("올바르지않은 기호입니다.");
-                    continue;
+                } catch (Exception e) {
+                    System.out.println("올바르지 않은 입력입니다.");
                 }
             }
-        } catch (Exception e) {
-            System.out.println("올바르지 않은 입력입니다.");
         }
     }
 }
-
